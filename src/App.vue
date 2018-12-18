3e9ad3d8b74b83818697a1a5f28ee462985020cf
Alli <allison.leong@salesforce.com>
2018-12-18 15:10:59 -0800

    Merging Luis's changes

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,18 @@ export default {
 </script>
 
 <style>
+html {
+    height: 100%;
+}
+
+body {
+    height: 100%;   
+    overflow: hidden;  /*makes the body non-scrollable (we will add scrolling to the sidebar and main content containers)*/
+    margin: 0px;  /*removes default style*/
+    display: flex;  /*enables flex content for its children*/
+    box-sizing: border-box;
+    border: blue solid 10px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
