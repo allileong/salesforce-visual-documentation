b48dc1f1d9b0e0ef3ba983a92904f00f568efb58
Alli <allison.leong@salesforce.com>
2018-12-18 13:48:01 -0800

    Create Sidebar Items

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="sidebar">
+      <SideBarItem v-bind:store="store" itemName="CLI"/>
+      <SideBarItem v-bind:store="store" itemName="Replay Debugger"/>
+      <SideBarItem v-bind:store="store" itemName="Apex LSP"/>
+    </div>
+    <div id="content">
+      <div>{{store.name}}</div>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld v-bind:store="store" msg="Welcome to Your Vue.js App"/>
+    </div>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SideBarItem from './components/SideBarItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    SideBarItem
+  },
+  data: function() {
+    return {
+      store: {
+        name: 'foo'
+      }
+    }
   }
 }
 </script>
@@ -24,5 +42,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  border: red 1px solid;
+}
+
+#sidebar {
+  height:100%;
+  width: 160px;
+  border: black solid 1px;
 }
 </style>
